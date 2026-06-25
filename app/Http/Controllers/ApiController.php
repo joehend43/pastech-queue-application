8cd45b84f09152a845d150778c6cb64d0914f8ab
Q-toel <joehendraaa@gmail.com>
2026-06-25 18:31:29 +0700

    Revert "undo dummy, doesn't work"
    This reverts commit 4830336c1d270f4c8c9758c070035413acb63816.

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -46,7 +46,11 @@ class ApiController extends Controller
             'last_login' => now()
         ]);
 
-        $dummy = Queue::withTrashed()->first();
+        // $dummy = Queue::withTrashed()->first();
+        $dummy = new Queue();
+        $dummy->id = 0;
+        $dummy->type = 'O';
+        $dummy->queue_number = 0;
         broadcast(new QueueCalled($dummy, $user->name, $user->status))->toOthers();
 
         return response()->json($user);
@@ -59,7 +63,11 @@ class ApiController extends Controller
             'status' => 'Offline'
         ]);
 
-        $dummy = Queue::withTrashed()->first();
+        // $dummy = Queue::withTrashed()->first();
+        $dummy = new Queue();
+        $dummy->id = 0;
+        $dummy->type = 'O';
+        $dummy->queue_number = 0;
         broadcast(new QueueCalled($dummy, $user->name, $user->status))->toOthers();
 
         return response()->json($user);
@@ -75,7 +83,11 @@ class ApiController extends Controller
         } else {
             $user->update(['status' => 'Online', 'last_login' => now()]);
         }
-        $dummy = Queue::withTrashed()->first();
+        // $dummy = Queue::withTrashed()->first();
+        $dummy = new Queue();
+        $dummy->id = 0;
+        $dummy->type = 'O';
+        $dummy->queue_number = 0;
         broadcast(new QueueCalled($dummy, $user->name, $user->status))->toOthers();
 
         return response()->json($user);
